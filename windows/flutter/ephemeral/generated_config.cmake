2f961cfe6378a78c57106217ba1729d6ae188e21
cristinaimbesi <102077083+cristinaimbesi@users.noreply.github.com>
2022-05-25 23:55:42 +0200

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\ASUS\\Documents\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\ASUS\\Desktop\\APPMEB\\meb_application" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\ASUS\\Documents\\flutter"
+  "PROJECT_DIR=C:\\Users\\ASUS\\Desktop\\APPMEB\\meb_application"
+  "FLUTTER_ROOT=C:\\Users\\ASUS\\Documents\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\ASUS\\Desktop\\APPMEB\\meb_application\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\ASUS\\Desktop\\APPMEB\\meb_application"
+  "FLUTTER_TARGET=C:\\Users\\ASUS\\Desktop\\APPMEB\\meb_application\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\ASUS\\Desktop\\APPMEB\\meb_application\\.dart_tool\\package_config.json"
+)
